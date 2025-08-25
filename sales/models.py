442abc75112964ea236d871603f443a928f85b18
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-25 13:12:59 -0500

    First commit

--- sales/models.py
+++ sales/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from ..products.models import Product
 
 # Create your models here.
 class SalesDetail(models.Model):
-    product = models
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='sales_details', null=False)
+    quantity = models.PositiveIntegerField(null=False, blank=False, default=1)
+    subtotal = models.DecimalField(max_digits=30, decimal_places=2, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.product.name} - {self.quantity} units"
+    
+class Sale(models.Model):
+    sale_detail = models.ForeignKey(SalesDetail, on_delete=models.PROTECT, related_name='sales', null=False)
+    date = models.DateTimeField(auto_now_add=True)
+    
