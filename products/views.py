487c04f4c048bb990cd56106ce1626dd67d22193
AndresDev20 <andresmarroquin887@gmail.com>
2025-09-01 18:08:28 -0500

    First commit

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def product_list(request):
-    return render(request, 'dashboard/products.html')
+    list_products = Product.objects.all()
+    return render(request, 'dashboard/products.html', {'list_products': list_products})
