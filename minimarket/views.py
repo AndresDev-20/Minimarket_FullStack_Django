b576c36c8def986baed5ce551a5cac5f90264f1f
AndresDev20 <andresmarroquin887@gmail.com>
2025-08-31 21:24:05 -0500

    First commti

--- minimarket/views.py
+++ minimarket/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
+    data = request.POST
+    if request.method == "POST":
+        username = data.get("username")
+        password = data.get("password")
+        # Aquí puedes agregar la lógica para autenticar al usuario
+        if username == "Andres.dev" and password == "1234":
+            return render(request, 'dashboard/dashboard.html', {})
+        else:
+            return render(request, 'index.html', {})
     return render(request, 'index.html', {})
