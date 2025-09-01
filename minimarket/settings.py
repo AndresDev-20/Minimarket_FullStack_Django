23903b26f25a3b11e366fbdaa15b20e3f23af92d
AndresDev20 <andresmarroquin887@gmail.com>
2025-09-01 15:12:26 -0500

    First commit

--- minimarket/settings.py
+++ minimarket/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'minimarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
