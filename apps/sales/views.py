700d24dddd0770673a8e288c3113ce3de2d07f51
AndresDev20 <andresmarroquin887@gmail.com>
2025-09-21 15:11:03 -0500

    New Update

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import redirect, render
 from .models import Sale, SalesDetail
 from .forms import SaleForm, SalesDetailForm
+from datetime import date, timedelta
 
 # Create your views here.
 def sales(request):
-    sales_list = Sale.objects.all()
-    return render(request, 'sale/sales.html', {'sales': sales_list})
+     filtro = request.GET.get('filter', 'all')
+     today = date.today()
+     if filtro == "day":
+        sales_list = Sale.objects.filter(date__date=today)
+     elif filtro == "week":
+        sales_list = Sale.objects.filter(date__week=today.isocalendar()[1], date__year=today.year)
+     elif filtro == "month":
+        sales_list = Sale.objects.filter(date__month=today.month, date__year=today.year)
+     elif filtro == "year":
+        sales_list = Sale.objects.filter(date__year=today.year)
+     else:
+         sales_list = Sale.objects.all()
+
+     return render(request, 'sale/sales.html', {
+        'sales': sales_list,
+        'active_filter': filtro
+    })
 
 # Create Sale
 def create_sale(request):
